8671f57b34549a99eb323bcbfef96bad0fc983b7
Whendel Muniz <142748618+WhendelM@users.noreply.github.com>
2023-12-06 19:26:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
+Console.WriteLine("Bem-Vindo ao Celular Nokia");
+Smartphone nokiacelular = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "12345678", memoria: 128);
+nokiacelular.Ligar();
+nokiacelular.InstalarAplicativo("Facebook");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Bem-Vindo ao Celular Nokia");
+Smartphone iphonecelular = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "87654321", memoria: 256);
+iphonecelular.Ligar();
+iphonecelular.InstalarAplicativo("Instagram");
+// Programa testado!
