b6cbd1aef9877288ed9174e3b8f93899f7824cf3
Whendel Muniz <142748618+WhendelM@users.noreply.github.com>
2023-12-06 19:26:46 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ nokiacelular.InstalarAplicativo("Facebook");
 
 Console.WriteLine("\n");
 
-Console.WriteLine("Bem-Vindo ao Celular Nokia");
+Console.WriteLine("Bem-Vindo ao Celular Iphone");
 Smartphone iphonecelular = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "87654321", memoria: 256);
 iphonecelular.Ligar();
 iphonecelular.InstalarAplicativo("Instagram");
